b9168060ca261890f9390fa5ab8e283bd15f0744
Kirill <kirilldrozd12@gmail.com>
2022-03-12 17:02:54 +0300

    Initial Commit

---
+++ snapKitAnimation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  snapKitAnimation
+//
+//  Created by Kirill Drozdov on 12.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
