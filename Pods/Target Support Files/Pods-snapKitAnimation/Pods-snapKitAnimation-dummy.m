b95913312b16239d53b238eed9e90ff031784cc0
Kirill <kirilldrozd12@gmail.com>
2022-03-12 23:22:17 +0300

    create

---
+++ Pods/Target Support Files/Pods-snapKitAnimation/Pods-snapKitAnimation-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_snapKitAnimation : NSObject
+@end
+@implementation PodsDummy_Pods_snapKitAnimation
+@end
--- snapKitAnimation/ViewController.swift
+++ snapKitAnimation/ViewController.swift
@@ -6,12 +6,74 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
 
+  var animatedView: UIView? // Animated View
+  var button: UIButton? // Button
+  var isOn: Bool = false // Button Flag
+
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+
+    animatedView = UIView()
+    animatedView?.backgroundColor = UIColor.red
+    self.view.addSubview(animatedView!)
+    animatedView?.snp.makeConstraints { (make) in
+      make.center.equalToSuperview()
+      make.width.equalTo(200)
+      make.height.equalTo(200)
+    }
+
+    button = UIButton()
+    button?.backgroundColor = UIColor.blue
+    button?.setTitle("button", for: .normal)
+    button?.addTarget(self, action: #selector(onButton), for: .touchUpInside)
+    self.view.addSubview(button!)
+    button?.snp.makeConstraints({ (make) in
+      make.top.equalTo(50)
+      make.centerX.equalToSuperview()
+    })
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+  @objc func onButton() {
+
+    if isOn == false { // если фолс
+      self.isOn.toggle() // change true
+      // reset constraint
+      animatedView?.snp.remakeConstraints({ (make) in
+        make.bottom.equalToSuperview().offset(-10)
+        make.centerX.equalToSuperview()
+        make.width.equalTo(50)
+        make.height.equalTo(50)
+      })
+      createAnimation()
+
+
+    } else {
+
+      self.isOn.toggle()
+      animatedView?.snp.remakeConstraints({ (make) in
+        make.center.equalToSuperview()
+        make.width.equalTo(200)
+        make.height.equalTo(200)
+      })
+
+      createAnimation()
+    }
+
+    // что бы не писать много раз
+    func createAnimation(){
+      UIView.animate(withDuration: 1.0) {
+        self.view.layoutIfNeeded()
+      }
+    }
   }
 
 
